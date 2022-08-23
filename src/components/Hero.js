d77af8f70bbad5e47b4f50ca3ecf225da98a2662
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-08-23 21:31:29 +0200

    Add Hero animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import headerImageOne from "../assets/bg/header_bg_1.jpg";
 import line from "../assets/bottom_line2.png";
 import logo from "../assets/icon/Logo.svg";
@@ -15,6 +16,35 @@ export default function Hero() {
   const heroImagePicker = () =>
 `../assets/bg/${heroImages[getRandomImage(0, 2)]}`;*/
   }
+  const logoAnimation = {
+    initial: {
+      rotateX: 95,
+      opacity: 0,
+    },
+    animate: {
+      rotateX: 0,
+      opacity: 1,
+      transition: {
+        duration: 1.5,
+        ease: "easeInOut",
+      },
+    },
+  };
+  const sloganAnimation = {
+    initial: {
+      opacity: 0,
+      y: -50,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 1,
+        duration: 1,
+        ease: "easeInOut",
+      },
+    },
+  };
 
   return (
     <section className="relative">
@@ -26,15 +56,25 @@ export default function Hero() {
       {/* left: 0; right: 0; margin-left: auto; margin-right: auto; */}
 
       <div className="absolute z-30 right-0 left-0 top-1/2 flex flex-col items-center">
-        <div>
-          <img src={logo} alt="logo" />
-        </div>
-        <div className="flex flex-col items-center">
+        <motion.img
+          src={logo}
+          alt="logo"
+          variants={logoAnimation}
+          initial="initial"
+          animate="animate"
+        />
+
+        <motion.div
+          className="flex flex-col items-center"
+          variants={sloganAnimation}
+          initial="initial"
+          animate="animate"
+        >
           <h2 className=" text-[color:white] place-self-center text-4xl uppercase tracking-widest">
             Have a good time
           </h2>
           <img src={line} alt="night club logo" />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
