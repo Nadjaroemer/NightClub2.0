47c2df219726e16853db253188b4a1c84bd8bc48
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-08-23 20:34:04 +0200

    Add BlogPost sektion

---
+++ src/components/BlogPost.js
@@ -0,0 +1,22 @@
+export default function BlogPost({
+  image,
+  headline,
+  author,
+  comments,
+  date,
+  text,
+}) {
+  return (
+    <div className="pl-8">
+      <img src={image} alt="blogpost" />
+      <h2 className="uppercase text-2xl pt-8 pb-4">{headline}</h2>
+      <div className="flex text-[#ff2a70] text-l pb-6">
+        <p className="uppercase ">by:</p>
+        <p>{author}/</p>
+        <p>{comments}/</p>
+        <p>{date}</p>
+      </div>
+      <p className="text-xl">{text}</p>
+    </div>
+  );
+}
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,10 +1,14 @@
 import Headline from "../components/Headline";
 import Testimonials from "../components/Testimonials";
+import BlogPost from "../components/BlogPost";
 
 import restaurantImageTo from "../assets/content-img/reastaurant_1.jpg";
 import restaurantImageOne from "../assets/content-img/thumb1.jpg";
 import restaurantImageThree from "../assets/content-img/thumb2.jpg";
-import testimonialThree from "../assets/content-img/testimonial_3.jpg"
+import testimonialThree from "../assets/content-img/testimonial_3.jpg";
+import blogPostOne from "../assets/content-img/blog_full1.jpg";
+import blogPostTwo from "../assets/content-img/blog_full2.jpg";
+import blogPostThree from "../assets/content-img/blog_full3.jpg";
 
 
 export default function Landingpage() {
@@ -23,12 +27,44 @@ export default function Landingpage() {
 
         <Headline headline="EVENTS OF THE MONTH" />
         <Headline headline="NIGHT CLUB GALLERY" />
-        <Headline headline="RECENT BLOG" />
 
         <Testimonials
         image={testimonialThree}
         name="Alex"
         text="There are many variations of passages of Lorem ipsum available, but the majority have suddered aæteration in som form, by injected humour, or randomised words which don*t look even slightly believable. If you are going to uyse a passage of Lorem Ipsum you ned to be sure there isn*t anything embarrising hidden in the middle of the text"/>
+        
+
+        <Headline headline="RECENT BLOG" />
+
+        <div className="flex pl-24 pr-24">
+        <BlogPost
+        image={blogPostOne}
+        headline="More than 20 Yea..."
+        author="Admin"
+        comments="3 Comments"
+        date="16 Nov 2018"
+        text="It is long established fact that a reader will be distracted by the readable content of a page when looking at its layout"
+        />
+
+        <BlogPost
+        image={blogPostTwo}
+        headline="More than 20 Yea..."
+        author="Admin"
+        comments="3 Comments"
+        date="16 Nov 2018"
+        text="It is long established fact that a reader will be distracted by the readable content of a page when looking at its layout"
+        />
+
+        <BlogPost
+        image={blogPostThree}
+        headline="More than 20 Yea..."
+        author="Admin"
+        comments="3 Comments"
+        date="16 Nov 2018"
+        text="It is long established fact that a reader will be distracted by the readable content of a page when looking at its layout"
+        />
+        </div>
+
         
         </div>
 
