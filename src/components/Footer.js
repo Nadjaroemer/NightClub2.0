d961885ddfa2755814e80be0d6d95723e216f60c
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-08-23 13:31:05 +0200

    Updated styling in footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,23 +9,23 @@ import {
 
 export default function Footer() {
   return (
-    <section className="flex justify-between sm:flex-wrap text-base">
+    <section className="flex justify-between items-center sm:flex-wrap text-base pb-24">
       <p className="capitalize">Stay Connected with us night Club</p>
 
       <div className="flex">
-        <div className="border-2 border-white w-14 h-14 flex justify-center items-center ">
+        <div className="border-2 border-white w-12 h-12 flex justify-center items-center ">
           <FaFacebookF size="1.5rem" />
         </div>
-        <div className="flex border-2 border-white w-14 h-14 justify-center items-center ml-8">
+        <div className="flex border-2 border-white w-12 h-12 justify-center items-center ml-8">
           <FaTwitter size="1.5rem" />
         </div>
-        <div className="flex border-2 border-white w-14 h-14 justify-center items-center ml-8">
+        <div className="flex border-2 border-white w-12 h-12 justify-center items-center ml-8">
           <FaGooglePlusG size="1.5rem" />
         </div>
-        <div className="flex border-2 border-white w-14 h-14 justify-center items-center ml-8">
+        <div className="flex border-2 border-white w-12 h-12 justify-center items-center ml-8">
           <FaSkype size="1.5rem" />
         </div>
-        <div className="flex border-2 border-white w-14 h-14 justify-center items-center ml-8">
+        <div className="flex border-2 border-white w-12 h-12 justify-center items-center ml-8">
           <FaBloggerB size="1.5rem" />
         </div>
       </div>
