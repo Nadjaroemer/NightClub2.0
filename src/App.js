7d629ac4e1017f08bd22ccf0b2f9545247b6222f
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-08-23 09:35:51 +0200

    Add Routes and Hero component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import Hero from "./components/Hero";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black text-red-900">
+      <BrowserRouter>
+        <Hero />
+        <nav className="bg-black">
+          <Link to="/">Root</Link>
+          <Link to="/about-us">About Us</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/events">Events</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/booking">Book Table</Link>
+          <Link to="/contact-us">Contact Us</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<div>root</div>} />
+          <Route path="/about-us" element={<div>About Us</div>} />
+          <Route path="/blog" element={<div>Blog</div>} />
+          <Route path="/events" element={<div>Events</div>} />
+          <Route path="/gallery" element={<div>Gallery</div>} />
+          <Route path="/booking" element={<div>Book a table</div>} />
+          <Route path="/contact-us" element={<div>Contact Us</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion";
+
+export default function Hero() {
+  const heroImages = ["header_bg_1.jpg", "header_bg_2.jpg"];
+
+  function getRandomImage(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  const heroImagePicker = () =>
+    `./src/assets/bg/${heroImages[getRandomImage(0, 2)]}`;
+
+  return (
+    <section className="relative">
+      <div className="bg-red">hey im red</div>
+      <img className="w-screen h-screen z-0" src={heroImagePicker()} />
+      {/* left: 0; right: 0; margin-left: auto; margin-right: auto; */}
+
+      <div className="absolute z-30 right-0 left-0 top-1/2 flex flex-col items-center">
+        <div>
+          <img src="./src/assets/icon/Logo_main.svg" />
+        </div>
+        <div className="flex flex-col items-center">
+          <h2 className=" text-[color:white] place-self-center text-2xl">
+            Have a good time
+          </h2>
+          <img src="./src/assets/bottom_line2.png" alt="night club logo" />
+        </div>
+      </div>
+    </section>
+  );
+}
