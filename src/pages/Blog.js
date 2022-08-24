2cb48d757bd83150a4ce979fd3f879d9c36d7459
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-08-24 21:27:46 +0200

    Fetch comments

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,40 +1,55 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const selectedBlogPostId = 1;
-
-const arr = [1, 2, 3, 4, 5];
-const filteredArr = arr.filter((element) => {
-  if (element === 2 || element === 4) {
-    return true;
-  }
-  return false;
-});
-console.log("filteredArr: ", filteredArr);
+const SELECTED_BLOG_POST_ID = 3;
 
 export default function Blog() {
   const [latestBlogPost, setLatestBlogPost] = useState();
+  const [comments, setComments] = useState();
 
   useEffect(() => {
     const fetchBlogPosts = () => {
       axios
         .get(
-          `${process.env.REACT_APP_NIGHTCLUB_APP_BASE_URL}/blogposts/${selectedBlogPostId}`
+          `${process.env.REACT_APP_NIGHTCLUB_APP_BASE_URL}/blogposts/${SELECTED_BLOG_POST_ID}`
         )
         .then((response) => {
           setLatestBlogPost(response.data);
         });
     };
 
-    const fetchComments = () => {};
+    const fetchComments = () => {
+      axios
+        .get(
+          `${process.env.REACT_APP_NIGHTCLUB_APP_BASE_URL}/blogposts/${SELECTED_BLOG_POST_ID}/comments`
+        )
+        .then((response) => {
+          setComments(response.data);
+        });
+    };
 
     fetchBlogPosts();
     fetchComments();
   }, [setLatestBlogPost]);
 
   return (
-    <div>
-      {latestBlogPost ? <div>Blog post id: {latestBlogPost.id}</div> : null}
-    </div>
+    <section>
+      <div>
+        {latestBlogPost ? <div>Blog post id: {latestBlogPost.id}</div> : null}
+      </div>
+
+      <div>
+        {comments
+          ? comments.map((comment) => {
+              return (
+                <div>
+                  <div>here is an image</div>
+                  {comment.content}
+                </div>
+              );
+            })
+          : null}
+      </div>
+    </section>
   );
 }
