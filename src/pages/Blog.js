63be694c60f342e2538508f6507fd1c956caf79e
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-08-24 20:48:04 +0200

    Fetching blogposts

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,6 +1,27 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Blog() {
-  return <div>Hello World</div>;
+  const [blogPosts, setBlogPosts] = useState();
+
+  useEffect(() => {
+    const fetchBlogPosts = () => {
+      axios
+        .get(`${process.env.REACT_APP_NIGHTCLUB_APP_BASE_URL}/blogposts`)
+        .then((response) => {
+          setBlogPosts(response.data);
+        });
+    };
+    fetchBlogPosts();
+  }, [setBlogPosts]);
+
+  return (
+    <div>
+      {blogPosts
+        ? blogPosts.map((blogPost) => {
+            return <div>Blog post id: {blogPost.id}</div>;
+          })
+        : null}
+    </div>
+  );
 }
