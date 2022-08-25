25d3c38978bec339b1dcc0648bc440a458c56078
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-08-25 21:25:44 +0200

    Updated comments form

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,8 +1,37 @@
+import axios from "axios";
+import { useState } from "react";
+
 import blogPostImage from "../assets/content-img/blog_full3.jpg";
 import commentImage from "../assets/content-img/testimonial_1.jpg";
 import Headline from "../components/Headline";
 
 export default function Blog(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [comment, setComment] = useState("");
+
+  const submitForm = () => {
+    if (props.latestBlogPost) {
+      // Set loading state
+      axios
+        .post(
+          `${process.env.REACT_APP_NIGHTCLUB_APP_BASE_URL}/blogposts/${props.latestBlogPost.id}/comments`,
+          {
+            name,
+            email,
+            content: comment,
+            date: new Date().toDateString(),
+          }
+        )
+        .then(() => {
+          // Reset fields
+          setName("");
+          setEmail("");
+          setComment("");
+        });
+    }
+  };
+
   return (
     <>
       <Headline headline="Blog Post" />
@@ -64,23 +93,39 @@ export default function Blog(props) {
           <form
             className="grid grid-cols-2 grid-rows-3 gap-8"
             onSubmit={(e) => {
-              console.log(e);
+              e.preventDefault();
+              submitForm();
             }}
           >
             <input
               className="border-2 p-2 bg-transparent md:h-20"
               type="text"
               placeholder="Your Name"
-            ></input>
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
             <input
               className="border-2 p-2 bg-transparent md:h-20"
               placeholder="Your Email"
-            ></input>
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
             <textarea
               className="border-2 p-2 bg-transparent col-span-2"
               placeholder="Comment"
-            ></textarea>
-            <button className="col-start-2 col-end-3 place-self-end p{t/b}-{10px} pl-8 pr-8 bg-transparent border-white uppercase border-y-4">
+              value={comment}
+              onChange={(e) => {
+                setComment(e.target.value);
+              }}
+            />
+            <button
+              className="col-start-2 col-end-3 place-self-end p{t/b}-{10px} pl-8 pr-8 bg-transparent border-white uppercase border-y-4"
+              type="submit"
+            >
               Submit
             </button>
           </form>
