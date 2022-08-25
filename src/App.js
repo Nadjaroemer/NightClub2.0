a816ab774c91cf2e99e30a9c01ea838c18aadac9
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-08-25 14:45:36 +0200

    Add mobilemenue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import axios from "axios";
+import { useMediaQuery } from "react-responsive";
 
 import Hero from "./components/Hero";
 import "./App.css";
@@ -8,10 +9,13 @@ import logo from "../src/assets/Logo.png";
 import Footer from "./components/Footer";
 import Landingpage from "./pages/Landingpage";
 import Blog from "./pages/Blog";
+import MobileMenu from "./components/Mobilemenu";
 
 const SELECTED_BLOG_POST_ID = 3;
 
 function App() {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 640px)" });
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [blogPosts, setBlogPosts] = useState();
   const [comments, setComments] = useState();
 
@@ -42,34 +46,87 @@ function App() {
     <div className="bg-black text-white font-['Ubuntu']">
       <BrowserRouter>
         <Hero />
-        <nav className="bg-black border-y-2 border-[#ff2a70] text-2xl uppercase ">
-          <ul className="flex justify-around pl-10 pr-10 mt-10 mb-10">
-            <li className="pl-36">
-              <img src={logo} alt="Logo" />
-            </li>
-            <li className="pl-48">
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about-us">About Us</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/events">Events</Link>
-            </li>
-            <li>
-              <Link to="/gallery">Gallery</Link>
-            </li>
-            <li>
-              <Link to="/booking">Book Table</Link>
-            </li>
-            <li className="pr-36">
-              <Link to="/contact-us">Contact Us</Link>
-            </li>
-          </ul>
-        </nav>
+
+        {isMobileMenuOpen ? (
+          <div className="fixed w-screen h-screen bg-black z-50">
+            <MobileMenu setIsMobileMenuOpen={setIsMobileMenuOpen} />
+          </div>
+        ) : undefined}
+
+        <div>
+          {isTabletOrMobile ? (
+            <button
+              className="pt-4"
+              onClick={() => {
+                setIsMobileMenuOpen(!isMobileMenuOpen);
+              }}
+            >
+              {isMobileMenuOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="white"
+                  fill-white="white"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          ) : (
+            <div>
+              <nav className="bg-black border-y-2 border-[#ff2a70] text-2xl uppercase ">
+                <ul className="flex justify-around pl-10 pr-10 mt-10 mb-10">
+                  <li className="pl-36">
+                    <img src={logo} alt="Logo" />
+                  </li>
+                  <li className="pl-48">
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about-us">About Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                  <li>
+                    <Link to="/events">Events</Link>
+                  </li>
+                  <li>
+                    <Link to="/gallery">Gallery</Link>
+                  </li>
+                  <li>
+                    <Link to="/booking">Book Table</Link>
+                  </li>
+                  <li className="pr-36">
+                    <Link to="/contact-us">Contact Us</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          )}
+        </div>
 
         <Routes>
           <Route path="/" element={<Landingpage blogPosts={blogPosts} />} />
---
+++ src/components/Mobilemenu.js
@@ -0,0 +1,52 @@
+import { Link } from "react-router-dom";
+export default function MobileMenu({ setIsMobileMenuOpen }) {
+  return (
+    <>
+      <nav>
+        <div>
+          <button
+            className="pt-4 pl-2"
+            onClick={() => {
+              setIsMobileMenuOpen(false);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="black"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <ul className="flex flex-col text-center text-4xl gap-3 h-full pt-12">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about-us">About Us</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/events">Events</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+          <li>
+            <Link to="/booking">Book Table</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
